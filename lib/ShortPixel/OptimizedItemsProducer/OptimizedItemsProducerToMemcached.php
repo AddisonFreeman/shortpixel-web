07ac53dc4593badaa61d4acc8dc7a53900e05e02
addisonfreeman <afree1423@gmail.com>
2017-12-27 14:20:19 -0600

    test public variables

--- lib/ShortPixel/OptimizedItemsProducer/OptimizedItemsProducerToMemcached.php
+++ lib/ShortPixel/OptimizedItemsProducer/OptimizedItemsProducerToMemcached.php
@@ -4,12 +4,13 @@ namespace ShortPixel\OptimizedItemsProducer;
 
 class OptimizedItemsProducerToMemcached extends \ShortPixel\OptimizedItemsProducer
 {
-	// private $mem;
+	public $mem;
 	
 	function init() {
 		// $this->$mem = new \Memcache;
 		$memcache = new \Memcache;
 		$memcache->addServer('localhost', 11211);
+		$this->mem = $memcache;
 	}
 
 }
