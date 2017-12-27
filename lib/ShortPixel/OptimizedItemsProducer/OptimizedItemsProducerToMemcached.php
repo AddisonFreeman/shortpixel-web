9d26015860451ef73fb37eaa4f18030823e8e88f
addisonfreeman <afree1423@gmail.com>
2017-12-27 14:27:03 -0600

    test memcache setter and getter

--- lib/ShortPixel/OptimizedItemsProducer/OptimizedItemsProducerToMemcached.php
+++ lib/ShortPixel/OptimizedItemsProducer/OptimizedItemsProducerToMemcached.php
@@ -10,6 +10,7 @@ class OptimizedItemsProducerToMemcached extends \ShortPixel\OptimizedItemsProduc
 		// $this->$mem = new \Memcache;
 		$memcache = new \Memcache;
 		$memcache->addServer('localhost', 11211);
+		$memcache->set('remaining', $this->total - $this->result);
 		$this->mem = $memcache;
 	}
 
