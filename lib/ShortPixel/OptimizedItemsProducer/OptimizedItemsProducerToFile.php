e92b6b2f8a9097b559cf5fd273cd6ba5061cf86f
addisonfreeman <afree1423@gmail.com>
2017-12-27 13:26:55 -0600

    dumb typo

--- lib/ShortPixel/OptimizedItemsProducer/OptimizedItemsProducerToFile.php
+++ lib/ShortPixel/OptimizedItemsProducer/OptimizedItemsProducerToFile.php
@@ -7,7 +7,7 @@ use \ShortPixel\OptimizedItemsProducer;
 class OptimizedItemsProducerToFile extends OptimizedItemsProducer
 {
 	public function printToFile() {
-		file_put_contents(".shortpixel-q", $this->aprint())	
+		file_put_contents(".shortpixel-q", $this->aprint());	
 	}
 	
 }
