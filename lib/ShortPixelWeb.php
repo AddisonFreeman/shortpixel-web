d9f10b4defebe0f531e73725c5ccf0fd84be514f
addisonfreeman <afree1423@gmail.com>
2017-12-27 23:12:53 -0600

    add value inline again

--- lib/ShortPixelWeb.php
+++ lib/ShortPixelWeb.php
@@ -350,7 +350,9 @@ class ShortPixelWeb
             $arr = (object) array(
                 'total' => 23
             );
-            die(json_encode($cmd->wait($timeLimit)->toFiles($folderPath)->total = '23'));
+            $res = $cmd->wait($timeLimit)->toFiles($folderPath));
+            $res->'total' = 23;
+            die(json_encode($res));
         }
         catch(\Exception $e) {
             die(json_encode(array("status" => array("code" => $e->getCode(), "message" => $e->getMessage()))));
