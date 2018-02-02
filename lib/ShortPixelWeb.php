4a58960ccf8dece67d92cacbd07dc3bfd3b64346
addisonfreeman <afree1423@gmail.com>
2018-02-02 16:51:26 -0600

    variable logging

--- lib/ShortPixelWeb.php
+++ lib/ShortPixelWeb.php
@@ -350,7 +350,7 @@ class ShortPixelWeb
                     //     // $memcacheResult = ['hello' => 'test'];    
                     // }
                     
-                    var_dump($memcacheResult);
+                    // var_dump($memcacheResult);
                     var_dump($memcacheResult->succeeded);
 
                     // die(json_encode($memcacheResult));
